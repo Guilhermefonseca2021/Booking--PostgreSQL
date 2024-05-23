a1a46949b1086ad528360067deb96ab632ddf333
Guilhermefonseca2021 <gsbloogs194@gmail.com>
2024-05-23 20:28:59 -0300

    connection stabilsih to database

---
+++ src/main/java/com/booking/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.booking.constant;
+/**
+ * @author guilhermefonseca2021
+ * @version 1.0
+ * @Guilhermefonseca2021
+ * @email gsbloogs728@gmail.com
+ * @since 11/1
+ */
+public class Constant {
+    public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String X_REQUESTED_WITH = "X-Requested-With";
+}
--- src/main/java/com/booking/service/ContactService.java
+++ src/main/java/com/booking/service/ContactService.java
@@ -1,22 +1,33 @@
 package com.booking.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.booking.model.Contact;
 import com.booking.repositories.ContactRepo;
+// import com.booking.constant.Constant.PHOTO_DIRECTORY;
 
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 @Service
 @Slf4j // the logs by here
 @Transactional(rollbackOn = Exception.class)
-@RequiredArgsConstructor // we gonna be doing a dependency injection here
+@RequiredArgsConstructor
+// @RequiredArgsConstructor // we gonna be doing a dependency injection here
 public class ContactService {
     private final ContactRepo contactRepo;
     public Page<Contact> getAllContact(int page, int size) {
@@ -30,10 +41,32 @@ public class ContactService {
     };
     public void deleteContact(Contact contact) {
 
-//     };
-//     public String uploadPhoto(String id, MultipartFile file) {
-//         Contact contact = getContactById(id);
-//         String photoUrl = null;
-//         contact.setPhotoUrl(photoUrl);
-//     }
+    };
+    public String uploadPhoto(String id, MultipartFile file) {
+        log.info("Saving picture for user ID: {}", id);
+        Contact contact = getContactById(id);
+        String photoUrl = photoFunction.apply(id, file);
+        contact.setPhotoUrl(photoUrl);
+        contactRepo.save(contact);
+        return photoUrl;
+    }; 
+
+    private final Function<String, String> fileExtension = filename -> Optional.of(filename).filter(name -> name.contains("."))
+            .map(name -> "." + name.substring(filename.lastIndexOf(".") + 1)).orElse(".png");
+
+    private final BiFunction<String, MultipartFile, String> photoFunction = (id, image) -> {
+        String filename = id + fileExtension.apply(image.getOriginalFilename());
+        String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+        try {
+            Path fileStorageLocation = Paths.get(PHOTO_DIRECTORY).toAbsolutePath().normalize();
+            if(!Files.exists(fileStorageLocation)) { Files.createDirectories(fileStorageLocation); }
+            Files.copy(image.getInputStream(), fileStorageLocation.resolve(filename), REPLACE_EXISTING);
+            // get the url from server.
+            return ServletUriComponentsBuilder
+                    .fromCurrentContextPath()
+                    .path("/contacts/image/" + filename).toUriString();
+        }catch (Exception exception) {
+            throw new RuntimeException("Unable to save image");
+        }
+    };
 }
